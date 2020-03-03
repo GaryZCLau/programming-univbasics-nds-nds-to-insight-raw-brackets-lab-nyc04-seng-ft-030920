258bcfe8e9cd596e4ccbb9fe06d87b213b391c1b
benign-fork-5056 <gareeeliu@gmail.com>
2020-03-03 02:50:15 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,13 +12,13 @@ def directors_totals(nds)
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
+row_index
+while row_index < nds.count do
+
+end
+
   # Be sure to return the result at the end!
-nds[:name]
+
   nil
 end
 
