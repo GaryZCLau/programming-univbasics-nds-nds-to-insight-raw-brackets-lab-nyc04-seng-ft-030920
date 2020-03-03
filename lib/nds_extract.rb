50e22ee8ac3a72d19494e36ee97b597db4af5471
benign-fork-5056 <gareeeliu@gmail.com>
2020-03-03 02:40:34 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,7 +4,7 @@ require 'directors_database'
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
-  #
+  pp nds
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
@@ -18,5 +18,13 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
+nds[:name]
   nil
 end
+
+# index = 0
+# movies = directors_database[0][:movies]
+# while index < movies.count do
+#   puts movies[index][:title]
+#   index += 1
+# end
